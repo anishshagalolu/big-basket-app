3e656c968d69ca66c1f0972d01b9b063e39912b4
anishshagalolu <anishshagalolu@gmail.com>
2025-05-20 09:45:28 +0530

    logout

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from './store';
+import styles from './SignIn.css'; // Import as module
 
 function SignIn() {
-    let {register,handleSubmit}=useForm()
-    let dispatch=useDispatch();
-    let navigate=useNavigate();
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    let myFunc=(data)=>{
-        dispatch(loginUser(data))
-        navigate('/Veg');
-    }
+  const myFunc = (data) => {
+    dispatch(loginUser(data));
+    navigate('/Veg');
+  };
+
   return (
-    <>
-    <h2>User Sign In</h2>
-    <form onSubmit={handleSubmit(myFunc)}>
-        <input type='text' placeholder='Username'{...register('username')}/>
-        <input type="password" placeholder='Password'{...register('password')} />
-        <button type='submit'>Sign In</button>
-    </form>
-    <p>
-        New User?<a href='SignUp'>Sign Up</a>
-    </p>
-    </>
-  )
+    <div className={styles.container}>
+      <h2>User Sign In</h2>
+      <form onSubmit={handleSubmit(myFunc)}>
+        <input
+          type="text"
+          placeholder="Username"
+          {...register('username')}
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register('password')}
+          className={styles.input}
+        />
+        <button type="submit" className={styles.button}>
+          Sign In
+        </button>
+      </form>
+      <p className={styles.text}>
+        New User? <a href="SignUp" className={styles.link}>Sign Up</a>
+      </p>
+    </div>
+  );
 }
 
 export default SignIn;
